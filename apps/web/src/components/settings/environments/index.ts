8186f4191838b4b2ba8b448191bbc1f50d56e61e
@mrubens <2600+mrubens@users.noreply.github.com>
2026-08-12 04:27:54 +0000

    chore: remove unused routing editor export

--- apps/web/src/components/settings/environments/index.ts
+++ apps/web/src/components/settings/environments/index.ts
@@ -1,5 +1,4 @@
 export { Environments } from './Environments';
-export { RoutingRulesEditor } from './RoutingRulesEditor';
 export { EnvironmentRoutingOverview } from './EnvironmentRoutingOverview';
 export { CreateEnvironmentPage } from './CreateEnvironmentPage';
 export { EditEnvironmentPage } from './EditEnvironmentPage';
